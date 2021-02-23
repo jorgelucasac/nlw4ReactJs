a7fe97e76ba9e48a37390dc59f6895e8f3ac0abe
jorgelucas <jorgelucasl91@gmail.com>
2021-02-23 18:58:40 -0300

    add botão do cronômetro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,14 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css'
 
+import Head from 'next/head';
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
