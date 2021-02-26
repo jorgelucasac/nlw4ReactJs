dce59c1083ac351b89cd991c475576057cb1e2e6
jorgelucas <jorgelucasl91@gmail.com>
2021-02-26 16:03:32 -0300

    alteraçõs de informaçõs para homologação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home(props: HomeProps) {
     >
       <div className={styles.container}>
         <Head>
-          <title>Início | move.it</title>
+          <title>Início | move.it - Homologação</title>
         </Head>
         <ExperienceBar />
         <CountdonwProvider>
